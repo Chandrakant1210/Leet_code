83ac3c4983a162113a9bfef8c4f80fdc6bd8aa7e
Chandrakant kumar <chandrakantkumar1210@gmail.com>
2026-07-24 05:55:05 +0530

    Time: 11 ms (33.12%), Space: 21.1 MB (48.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mp={}
-        n=len(nums)
+        mp=defaultdict(int)
         for x in nums:
-            if x in mp:
-                mp[x]+=1
-                if(mp[x]>n//2):
-                    return x
-            else:
-                mp[x]=1
-        else:
-            return nums[0]
-        
+            mp[x]+=1
+            if mp[x]>len(nums)//2:
+                return x
+        return nums[0]
