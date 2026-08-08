c7b0395772239874696647654030a68a8ead1786
Chandrakant kumar <chandrakantkumar1210@gmail.com>
2026-08-08 07:49:58 +0530

    Time: 3 ms (18.74%), Space: 71.8 MB (53.62%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int c=0; 
+       int m=INT_MIN;
+       for (int i=0;i<nums.size();i++){
+        c+=nums[i];
+        m=max(m,c);
+        if(c<0)
+        c=0;
+       }
+       return m;
+    }
+};
